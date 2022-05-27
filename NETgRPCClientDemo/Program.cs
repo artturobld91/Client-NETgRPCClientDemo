7ab2eaec3447a043efee18f75db0d57daefb62de
Arturo_Balderas <Arturo_Balderas@epam.com>
2022-05-27 09:07:33 -0500

    Add project files.

---
+++ NETgRPCClientDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcServiceDemo;
+
+Console.WriteLine("Hello gRPC!");
+
+var channel = GrpcChannel.ForAddress("https://localhost:7024");
+var client = new Greeter.GreeterClient(channel);
+
+var response = await client.SayHelloAsync(
+    new HelloRequest
+    {
+        Name = "Art"
+    }
+);
+
+Console.WriteLine($"Response size (Bytes): {response.CalculateSize().ToString()}");
+Console.WriteLine($"Response from server: {response.Message}");
