9efb2cb26cf0955b41ff07b566a7df6d8133f03b
Arturo_Balderas <Arturo_Balderas@epam.com>
2022-05-30 11:45:46 -0500

    Added reference to demo.proto service.

--- NETgRPCClientDemo/Program.cs
+++ NETgRPCClientDemo/Program.cs
@@ -1,6 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Grpc.Net.Client;
 using GrpcServiceDemo;
+using NETGrpcServiceDemo.Protos;
 
 Console.WriteLine("Hello gRPC!");
 
@@ -16,3 +17,18 @@ var response = await client.SayHelloAsync(
 
 Console.WriteLine($"Response size (Bytes): {response.CalculateSize().ToString()}");
 Console.WriteLine($"Response from server: {response.Message}");
+
+Console.WriteLine("Demo gRPC!");
+
+var channeldemo = GrpcChannel.ForAddress("https://localhost:7024");
+var clientdemo = new Demo.DemoClient(channeldemo);
+
+var responsedemo = await clientdemo.DemoRPCAsync(
+    new DemoRequest
+    {
+        DesiredResults = 2
+    }
+);
+
+Console.WriteLine($"Demo Response size (Bytes): {responsedemo.CalculateSize().ToString()}");
+Console.WriteLine($"Demo Response from server: {responsedemo.DemoList}");
